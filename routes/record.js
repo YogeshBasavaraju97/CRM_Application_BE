32149cff2e889ef940b8420d48286d0c7c2e1672
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2025-04-07 13:52:45 +0530

    import name changed

--- routes/record.js
+++ routes/record.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/user");
+const User = require("../models/User");
 const userAuth = require("../middleware/auth");
 const Lead = require("../models/Lead");
 
